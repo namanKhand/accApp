5ecd68204df0d769f458d879a52ce45cf8657561
shreyakoli18 <shreyakoli0718@gmail.com>
2025-12-09 16:42:09 -0500

    Enable back buttons on all navigation screens

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useApp } from '../context/AppContext';
+import { COLORS } from '../constants/colors';
 import LaunchScreen from '../screens/LaunchScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import LoginSignupScreen from '../screens/LoginSignupScreen';
@@ -35,22 +36,77 @@ export const RootNavigator = () => {
   }
 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: COLORS.background,
+        },
+        headerTintColor: COLORS.text,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerShadowVisible: false,
+      }}
+    >
       {!user ? (
         <>
-          <Stack.Screen name="Launch" component={LaunchScreen} />
-          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-          <Stack.Screen name="LoginSignup" component={LoginSignupScreen} />
-          <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-          <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+          <Stack.Screen
+            name="Launch"
+            component={LaunchScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Onboarding"
+            component={OnboardingScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LoginSignup"
+            component={LoginSignupScreen}
+            options={{
+              title: 'Sign In',
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="CreateAccount"
+            component={CreateAccountScreen}
+            options={{ title: 'Create Account' }}
+          />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+            options={{ title: 'Reset Password' }}
+          />
+          <Stack.Screen
+            name="ResetPassword"
+            component={ResetPasswordScreen}
+            options={{ title: 'Reset Password' }}
+          />
         </>
       ) : (
         <>
-          <Stack.Screen name="Main" component={MainTabs} />
-          <Stack.Screen name="GoalSetup" component={GoalSetupScreen} />
-          <Stack.Screen name="InviteFriend" component={InviteFriendScreen} />
-          <Stack.Screen name="ContactUs" component={ContactUsScreen} />
+          <Stack.Screen
+            name="Main"
+            component={MainTabs}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="GoalSetup"
+            component={GoalSetupScreen}
+            options={{ title: 'Set Your Goal' }}
+          />
+          <Stack.Screen
+            name="InviteFriend"
+            component={InviteFriendScreen}
+            options={{ title: 'Invite Partner' }}
+          />
+          <Stack.Screen
+            name="ContactUs"
+            component={ContactUsScreen}
+            options={{ title: 'Contact Us' }}
+          />
         </>
       )}
     </Stack.Navigator>
